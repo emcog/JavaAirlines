4ae3166d675155bd4e70ecc751c7ccc93bf9fa09
emcog <robin.leonard@gmail.com>
2021-02-12 16:18:10 +0000

    pilot first test passed

---
+++ src/main/java/Pilot.java
@@ -0,0 +1,13 @@
+public class Pilot {
+
+    private String name;
+
+//    constructor
+    public Pilot(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+}
